c21690831248a9e120e2ce11f6829e0dd1746ad9
yogesh0731 <89070927+yogesh0731@users.noreply.github.com>
2021-08-17 20:20:32 +0530

    Create timetaken.java

---
+++ timetaken.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     float ravi=16/3; 
+     float kumar=8; 
+     float both=40/3; 
+     System.out.print((110*3)/40);
+	}
+
+}
