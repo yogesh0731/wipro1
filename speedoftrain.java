a4a7e3ceb01cf92bbc5d5abd97001eb51bc234de
yogesh0731 <89070927+yogesh0731@users.noreply.github.com>
2021-08-17 20:20:00 +0530

    Create speedoftrain.java

---
+++ speedoftrain.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class  SpeedofTrain{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in); 
+    int x=sc.nextInt(); 
+    int y=sc.nextInt(); 
+    int speed=(x*18)/(y*5); 
+    System.out.print(speed+5);
+	}
+
+}
