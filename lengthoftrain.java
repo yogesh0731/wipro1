ec12aecd51bd5b8c1df2f58f91a0c98e9afda1ed
yogesh0731 <89070927+yogesh0731@users.noreply.github.com>
2021-08-17 17:23:38 +0530

    Create lengthoftrain.java

---
+++ lengthoftrain.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class LengthofTrain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in); 
+    int x=sc.nextInt(); 
+    int y=sc.nextInt();
+    int length=(x*1000*y)/3600;
+    System.out.print(length);
+	}
+
+}
